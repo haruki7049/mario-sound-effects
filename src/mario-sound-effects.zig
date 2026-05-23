d6f86895d18263dc66acfad221e274daac808bbe
haruki7049 <tontonkirikiri@gmail.com>
2026-05-23 14:16:00 +0900

    Update

--- src/mario-sound-effects.zig
+++ src/mario-sound-effects.zig
@@ -11,25 +11,35 @@ pub fn gen(allocator: std.mem.Allocator) !lightmix.Wave(f64) {
     const channels = 1;
     const volume: f64 = 0.25;
 
-    var _4_4_sawtooth = try Sawtooth.gen(
+    var _4_4_c4_sawtooth = try Sawtooth.gen(
         f64,
         allocator,
         Scale.gen(.{ .code = .c, .octave = 4 }),
         sample_rate,
         volume,
     );
-    try _4_4_sawtooth.filter(Filters.decay);
-    defer _4_4_sawtooth.deinit();
+    try _4_4_c4_sawtooth.filter(Filters.decay);
+    defer _4_4_c4_sawtooth.deinit();
+
+    var _4_4_e4_sawtooth = try Sawtooth.gen(
+        f64,
+        allocator,
+        Scale.gen(.{ .code = .e, .octave = 4 }),
+        sample_rate,
+        volume,
+    );
+    try _4_4_e4_sawtooth.filter(Filters.decay);
+    defer _4_4_e4_sawtooth.deinit();
 
     return try Splitter.gen(
         f64,
         allocator,
         44100 * 2,
         &.{
-            _4_4_sawtooth,
-            _4_4_sawtooth,
-            _4_4_sawtooth,
-            _4_4_sawtooth,
+            _4_4_c4_sawtooth,
+            _4_4_e4_sawtooth,
+            _4_4_c4_sawtooth,
+            _4_4_e4_sawtooth,
         },
         sample_rate,
         channels,
